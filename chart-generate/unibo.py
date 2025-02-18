6a0dd25227732d13a5fec1d3ebe33f611950996f
Alessandro <alessandro.gentilini@gmail.com>
2025-02-18 21:43:39 +0100

    refactory

--- chart-generate/unibo.py
+++ chart-generate/unibo.py
@@ -1,17 +1,12 @@
 import json
 
-# Open the JSON file
 with open('./out/chart.json') as file:
-    # Load the JSON data
     data = json.load(file)
 
-# Print the contents of the JSON file
-#print(data)
-
-def get_rank(field):
+def rank(field):
     return field['rank'].split('/')[-1]
 
-def get_name(field,language='it'):
+def name(field,language='it'):
     for f in field['altLabel']:
         if f['language']==language:
             return f['value']
@@ -32,14 +27,18 @@ def end(field):
         result = float(field["hasEnd"]["inMYA"]["value"])
     return result    
 
-# Print all the topconcept fields
+eoni = []
+ere = []
+periodi = []
+
 for L1 in data['hasTopConcept']:
-    print(get_name(L1),'\t',get_rank(L1),'\t',begin(L1),'->',end(L1))
+    print(name(L1),'\t',rank(L1),'\t',begin(L1),'->',end(L1))
+    eoni.append({'name':name(L1), 'rank':rank(L1),'begin':begin(L1), 'end':end(L1)})
     for L2 in L1['narrower']:
-        print('\t',get_name(L2),'\t',get_rank(L2),'\t',begin(L2),'->',end(L2))
+        print('\t',name(L2),'\t',rank(L2),'\t',begin(L2),'->',end(L2))
         if 'narrower' in L2:
             for L3 in L2['narrower']:
-                print('\t\t',get_name(L3),'\t',get_rank(L3),'\t',begin(L3),'->',end(L3))
+                print('\t\t',name(L3),'\t',rank(L3),'\t',begin(L3),'->',end(L3))
 
     # eone
     # era
