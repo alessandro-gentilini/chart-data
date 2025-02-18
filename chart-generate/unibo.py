99c1c956482068b965b39f283763c8a6dc9d2f93
Alessandro <alessandro.gentilini@gmail.com>
2025-02-18 21:48:44 +0100

    add colors

--- chart-generate/unibo.py
+++ chart-generate/unibo.py
@@ -25,19 +25,22 @@ def end(field):
         result = float(field["hasEnd"]["inMYA"])
     except:
         result = float(field["hasEnd"]["inMYA"]["value"])
-    return result    
+    return result   
+
+def color(field):
+    return field['color']
 
 eoni = []
 ere = []
 periodi = []
 
 for L1 in data['hasTopConcept']:
-    eoni.append({'name':name(L1), 'rank':rank(L1),'begin':begin(L1), 'end':end(L1)})
+    eoni.append({'name':name(L1), 'rank':rank(L1),'begin':begin(L1), 'end':end(L1), 'color':color(L1)})
     for L2 in L1['narrower']:
-        ere.append({'name':name(L2), 'rank':rank(L2),'begin':begin(L2), 'end':end(L2)})
+        ere.append({'name':name(L2), 'rank':rank(L2),'begin':begin(L2), 'end':end(L2), 'color':color(L1)})
         if 'narrower' in L2:
             for L3 in L2['narrower']:
-                periodi.append({'name':name(L2), 'rank':rank(L2),'begin':begin(L2), 'end':end(L2)})
+                periodi.append({'name':name(L2), 'rank':rank(L2),'begin':begin(L2), 'end':end(L2), 'color':color(L1)})
 
                 
 
